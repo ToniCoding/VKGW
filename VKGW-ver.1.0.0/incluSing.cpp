26e7094646f2ab3be4ae052ab94a10912d03a9a4
Antonio <55436023+ToniCoding@users.noreply.github.com>
2023-03-08 15:39:27 +0100

    Add files via upload

---
+++ VKGW-ver.1.0.0/incluSing.cpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// Includes + Using.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::to_string;
+using std::system;
